baba711606dca531a594622d76f795d4f0703e17
vmassol <vincent@massol.net>
2007-11-02 12:30:19 +0000

    XWIKI-1832: Page content can be accessed using XMLRPC even when not logged in and the page is protected
    Note: I've added a rights check but in the future we should instead use the XWiki public API (which has all the checks). However because we can do this we'll need to augment the public API as it's missing a few methods.
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-products/xwiki-enterprise/trunk@5606 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/xmlrpc-tests/src/test/it/com/xpn/xwiki/it/xmlrpc/AnonymousAccessTest.java
+++ distribution-test/xmlrpc-tests/src/test/it/com/xpn/xwiki/it/xmlrpc/AnonymousAccessTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.xpn.xwiki.xmlrpc.client.XWikiClient;
 import com.xpn.xwiki.xmlrpc.client.SwizzleXWikiClient;
+import com.xpn.xwiki.xmlrpc.client.XWikiClientException;
 import com.xpn.xwiki.xmlrpc.model.PageSummary;
 import com.xpn.xwiki.xmlrpc.model.SpaceSummary;
 
@@ -21,18 +22,34 @@ public class AnonymousAccessTest extends TestCase
         rpc = new SwizzleXWikiClient("http://127.0.0.1:8080/xwiki/xmlrpc");
     }
 
-    public void testReadAllPages() throws Exception
+    public void testReadSomePagesWhenNotLoggedIn() throws Exception
     {
         List spaces = rpc.getSpaces();
         for (int i = 0; i < spaces.size(); i++) {
         	SpaceSummary spaceSummary = (SpaceSummary)spaces.get(i);
             String key = spaceSummary.getKey();
-            List pages = rpc.getPages(key);
-            for (int j = 0; j < pages.size(); j++) {
-                PageSummary pageSummary = (PageSummary)pages.get(j);
-                String id = pageSummary.getId();
-                rpc.getPage(id);
+
+            // Only read pages from the Main space in this test since we're sure Guest users
+            // are allowed to read them.
+            if (key.equals("Main")) {
+                List pages = rpc.getPages(key);
+                for (int j = 0; j < pages.size(); j++) {
+                    PageSummary pageSummary = (PageSummary)pages.get(j);
+                    String id = pageSummary.getId();
+                    rpc.getPage(id);
+                }
             }
+        }
+    }
+
+    public void testReadUnauthorizedPage() throws Exception
+    {
+        try {
+            rpc.getPage("Scheduler.WebHome");
+            fail("Should have thrown an exception here");
+        } catch (XWikiClientException expected) {
+            assertTrue(expected.getMessage().contains(
+                "Access to document Scheduler.WebHome has been denied to user XWiki.XWikiGuest"));
         }
     }
 }

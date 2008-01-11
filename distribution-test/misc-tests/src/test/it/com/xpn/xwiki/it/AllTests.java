e34fe6b6b6c64689b37bb52da12f5617b89c4966
vmassol <vincent@massol.net>
2008-01-11 14:01:44 +0000

    XWIKI-2002: PDF export not working anymore (Avalon dependency missing)
    Fixed wrong package directory (due to copy paste)
    
    Merged from trunk (rev 6779)
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-products/xwiki-enterprise/branches/xwiki-enterprise-1.2@6781 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/misc-tests/src/test/it/com/xpn/xwiki/it/AllTests.java
+++ distribution-test/misc-tests/src/test/it/com/xpn/xwiki/it/AllTests.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package com.xpn.xwiki.it.xmlrpc;
+package com.xpn.xwiki.it;
 
 import junit.framework.Test;
 import junit.framework.TestCase;

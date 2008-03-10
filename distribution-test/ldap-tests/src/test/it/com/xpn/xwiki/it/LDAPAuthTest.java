92c6182d06d9cb84da8b5f55c0151d36d48beb83
tmortagne <thomas.mortagne@xwiki.com>
2008-03-10 11:48:40 +0000

    Add functional test to validate LDAP group_mapping property
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/xwiki-products/xwiki-enterprise/branches/xwiki-enterprise-1.3@8339 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/ldap-tests/src/test/it/com/xpn/xwiki/it/LDAPAuthTest.java
+++ distribution-test/ldap-tests/src/test/it/com/xpn/xwiki/it/LDAPAuthTest.java
@@ -97,6 +97,13 @@ public class LDAPAuthTest extends AbstractXWikiTestCase
 
         assertTrue(XWikiLDAPTestSetup.WILLIAMBUSH_UID + " user has not been authenticated",
             isAuthenticated());
+        
+        // ///////////////////
+        // Validate user is added in AdminGroup as it configured in group_mapping property
+        
+        open("/xwiki/bin/view/XWiki/XWikiAdminGroup");
+        
+        assertTextPresent("XWiki." + XWikiLDAPTestSetup.WILLIAMBUSH_UID);
     }
 
     /**

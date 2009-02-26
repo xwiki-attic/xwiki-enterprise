e236eeefb110eaae0956bf353045877e87603f33
lucaa <lucaa@xwiki.com>
2009-02-26 19:13:41 +0000

    Added tests for the link plugin: adding, editing and removing links. Merged from trunk (r17071).
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/branches/xwiki-enterprise-1.8@17072 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/wysiwyg-tests/src/test/it/com/xpn/xwiki/it/selenium/AllTests.java
+++ distribution-test/wysiwyg-tests/src/test/it/com/xpn/xwiki/it/selenium/AllTests.java
@@ -45,6 +45,7 @@ public class AllTests extends TestCase
         TestSuite suite = new TestSuite();
 
         addTestCase(suite, StandardFeaturesTest.class);
+        addTestCase(suite, LinkPluginTest.class);
         addTestCase(suite, ListSupportTest.class);
         addTestCase(suite, MacroSupportTest.class);
 

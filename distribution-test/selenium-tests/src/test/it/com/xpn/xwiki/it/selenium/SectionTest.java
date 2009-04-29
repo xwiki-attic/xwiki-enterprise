f0e975206b0a7a3a3f225665b3f9f7d085a85f48
sdumitriu <sergiu@xwiki.com>
2009-04-29 03:03:25 +0000

    [misc] Improved selenium test
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/trunk@19181 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/SectionTest.java
+++ distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/SectionTest.java
@@ -8,7 +8,7 @@ import com.xpn.xwiki.it.selenium.framework.XWikiTestSuite;
 
 /**
  * Tests the document edit section feature
- *
+ * 
  * @version $Id: $
  */
 public class SectionTest extends AbstractXWikiTestCase
@@ -25,35 +25,43 @@ public class SectionTest extends AbstractXWikiTestCase
     {
         super.setUp();
         loginAsAdmin();
-        open("Sandbox", "WebHome");
-        assertTextPresent("Training Zone");
-        assertTextPresent("Heading 1");
+        open("Sandbox", "WebHome", "edit", "editor=wiki");
+        createPage("Test", "SectionEditing", "1 First section\nSection 1 content\n\n"
+            + "1 Second section\nSection 2 content\n\n1.1 Subsection\nSubsection content\n\n"
+            + "1 Third section\nSection 3 content");
+        assertTextPresent("First section");
+        assertTextPresent("Subsection content");
+    }
+
+    @Override
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+        deletePage("Test", "SectionEditing");
     }
 
     /**
      * Verify edit section is working in wiki editor
      */
     public void testSectionEditInWikiEditor()
-    { 
-        clickLinkWithLocator("//div[@id='xwikicontent']/span[2]/a"); // Edit the second section
-        clickLinkWithText("Wiki");
-        assertTextNotPresent("Training Zone");
-        assertTextPresent("Heading 1");
-        clickEditSaveAndContinue();
-        assertTextNotPresent("Training Zone");
-        assertTextPresent("Heading 1");
-    }
-    
-    /**
-     * Verify edit section is working in wysiwyg editor
-     */    
-    public void testSectionEditInWysiwygEditor()
     {
         clickLinkWithLocator("//div[@id='xwikicontent']/span[2]/a"); // Edit the second section
-        assertTextNotPresent("Training Zone");
-        assertTextPresent("Heading 1");
-        clickEditSaveAndContinue();
-        assertTextNotPresent("Training Zone");
-        assertTextPresent("Heading 1");
+        clickLinkWithText("Wiki");
+        assertTextNotPresent("First section");
+        assertTextPresent("Second section");
+        assertTextPresent("Subsection content");
+        assertTextNotPresent("Third section");
+    }
+
+    /**
+     * Verify edit section is working in wysiwyg editor
+     */
+    public void testSectionEditInWysiwygEditor()
+    {
+        clickLinkWithLocator("//div[@id='xwikicontent']/span[3]/a"); // Edit the subsection
+        assertTextNotPresent("First section");
+        assertTextNotPresent("Second section");
+        assertTextPresent("Subsection content");
+        assertTextNotPresent("Third section");
     }
 }

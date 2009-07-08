dfcf0673f3718a365074875eaae3e2f7bb7a8934
jvdrean <jv@drean.org>
2009-07-08 13:35:52 +0000

    XWIKI-2881 : Implement Section editing
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/trunk@21854 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/SectionTest.java
+++ distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/SectionTest.java
@@ -13,8 +13,6 @@ import com.xpn.xwiki.it.selenium.framework.XWikiTestSuite;
  */
 public class SectionTest extends AbstractXWikiTestCase
 {
-    private static final String SYNTAX = "xwiki/1.0";
-    
     public static Test suite()
     {
         XWikiTestSuite suite = new XWikiTestSuite("Tests the document edit section feature");
@@ -27,43 +25,107 @@ public class SectionTest extends AbstractXWikiTestCase
     {
         super.setUp();
         loginAsAdmin();
-        open("Sandbox", "WebHome", "edit", "editor=wiki");
-        createPage("Test", "SectionEditing", "1 First section\nSection 1 content\n\n"
-            + "1 Second section\nSection 2 content\n\n1.1 Subsection\nSubsection content\n\n"
-            + "1 Third section\nSection 3 content", SYNTAX);
-        assertTextPresent("First section");
-        assertTextPresent("Subsection content");
-    }
-
-    @Override
-    protected void tearDown() throws Exception
-    {
-        super.tearDown();
-        deletePage("Test", "SectionEditing");
     }
 
     /**
-     * Verify edit section is working in wiki editor
+     * Verify edit section is working in wiki editor (xwiki/1.0).
+     * XWIKI-174 : Sectional editing.
      */
     public void testSectionEditInWikiEditor()
     {
+        open("Sandbox", "WebHome", "edit", "editor=wiki");        
+        createPage("Test", "SectionEditing", "1 First section\nSection 1 content\n\n"
+            + "1 Second section\nSection 2 content\n\n1.1 Subsection\nSubsection content\n\n"
+            + "1 Third section\nSection 3 content", "xwiki/1.0");        
         clickLinkWithLocator("//div[@id='xwikicontent']/span[2]/a"); // Edit the second section
         clickLinkWithText("Wiki");
         assertTextNotPresent("First section");
         assertTextPresent("Second section");
-        assertTextPresent("Subsection content");
+        assertTextPresent("Subsection");
         assertTextNotPresent("Third section");
     }
 
     /**
-     * Verify edit section is working in wysiwyg editor
+     * Verify edit section is working in wysiwyg editor (xwiki/1.0).
+     * XWIKI-174 : Sectional editing.
      */
     public void testSectionEditInWysiwygEditor()
     {
+        open("Test", "SectionEditing");
         clickLinkWithLocator("//div[@id='xwikicontent']/span[3]/a"); // Edit the subsection
         assertTextNotPresent("First section");
         assertTextNotPresent("Second section");
-        assertTextPresent("Subsection content");
+        assertTextPresent("Subsection");
+        assertTextNotPresent("Third section");        
+    }
+    
+    /**
+     * Verify section save does not override the whole document content (xwiki/1.0).
+     * XWIKI-4033: When saving after section edit entire page is overwritten.
+     */
+    public void testSectionSaveDoesNotOverrideTheWholeContent()
+    {
+        open("Test", "SectionEditing");
+        clickLinkWithLocator("//div[@id='xwikicontent']/span[3]/a"); // Edit the subsection
+        clickLinkWithText("Wiki");
+        clickEditSaveAndView();
+        assertTextPresent("First section");
+        assertTextPresent("Second section");
+        assertTextPresent("Subsection");
+        assertTextPresent("Third section");
+        deletePage("Test", "SectionEditing");
+    }
+
+    /**
+     * Verify edit section is working in wiki editor (xwiki/2.0).
+     * XWIKI-2881 : Implement Section editing.
+     */
+    public void testSectionEditInWikiEditor_syntax20()
+    {
+        createPage("Test", "SectionEditingIncluded", 
+            "== Included section ==\nFirst Included section content\n{{velocity wiki=true}}\n"
+                + "#foreach($h in ['First', 'Second'])\n== $h generated section ==\n\n$h generated paragraph\n"
+                + "#end\n{{velocity}}\n");
+        createPage("Test", "SectionEditing20", "= First section =\nSection 1 content\n\n"
+            + "= Second section =\nSection 2 content\n\n== Subsection ==\nSubsection content\n\n"
+            + "{{include document='Test.SectionEditingIncluded'/}}\n\n" + "= Third section =\nSection 3 content",
+            "xwiki/2.0");    
+        clickLinkWithLocator("//div[@id='xwikicontent']/span[2]/a"); // Edit the second section
+        clickLinkWithText("Wiki");
+        assertTextNotPresent("First section");
+        assertTextPresent("Second section");
+        assertTextPresent("Subsection");
         assertTextNotPresent("Third section");
+    }
+
+    /**
+     * Verify edit section is working in wysiwyg editor (xwiki/2.0).
+     * XWIKI-2881 : Implement Section editing.
+     */
+    public void testSectionEditInWysiwygEditor_syntax20()
+    {
+        open("Test", "SectionEditing20");        
+        clickLinkWithLocator("//div[@id='xwikicontent']/span[4]/a"); // Edit the last section
+        assertTextNotPresent("First section");
+        assertTextNotPresent("Second section");
+        assertTextNotPresent("Subsection");
+        assertTextPresent("Third section");        
+    }
+    
+    /**
+     * Verify section save does not override the whole document content (xwiki/2.0).
+     * XWIKI-4033: When saving after section edit entire page is overwritten.
+     */
+    public void testSectionSaveDoesNotOverrideTheWholeContent_syntax20()
+    {        
+        open("Test", "SectionEditing20");
+        clickLinkWithLocator("//div[@id='xwikicontent']/span[4]/a"); // Edit the last section
+        clickLinkWithText("Wiki");
+        clickEditSaveAndView();
+        assertTextPresent("First section");
+        assertTextPresent("Second section");
+        assertTextPresent("Third section");
+        deletePage("Test", "SectionEditing20");
+        deletePage("Test", "SectionEditingIncluded");
     }
 }

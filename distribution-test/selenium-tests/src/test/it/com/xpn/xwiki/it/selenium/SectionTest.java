eb73bb4c9fdc7a93acb54be0c7dc7c26fefd21d2
jvdrean <jv@drean.org>
2009-08-12 16:23:53 +0000

    [build] Refactored to make each test independent from the others.
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/trunk@22624 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/SectionTest.java
+++ distribution-test/selenium-tests/src/test/it/com/xpn/xwiki/it/selenium/SectionTest.java
@@ -27,16 +27,31 @@ public class SectionTest extends AbstractXWikiTestCase
         loginAsAdmin();
     }
 
-    /**
-     * Verify edit section is working in wiki editor (xwiki/1.0).
-     * XWIKI-174 : Sectional editing.
-     */
-    public void testSectionEditInWikiEditor()
-    {
-        open("Sandbox", "WebHome", "edit", "editor=wiki");        
+    protected void initXWiki10Env()
+    {       
         createPage("Test", "SectionEditing", "1 First section\nSection 1 content\n\n"
             + "1 Second section\nSection 2 content\n\n1.1 Subsection\nSubsection content\n\n"
             + "1 Third section\nSection 3 content", "xwiki/1.0");        
+    }
+
+    protected void initXWiki20Env()
+    {
+        createPage("Test", "SectionEditingIncluded",
+            "== Included section ==\nFirst Included section content\n{{velocity wiki=true}}\n"
+                + "#foreach($h in ['First', 'Second'])\n== $h generated section ==\n\n$h generated paragraph\n"
+                + "#end\n{{velocity}}\n");        
+        createPage("Test", "SectionEditing20", "= First section =\nSection 1 content\n\n"
+            + "= Second section =\nSection 2 content\n\n== Subsection ==\nSubsection content\n\n"
+            + "{{include document='Test.SectionEditingIncluded'/}}\n\n" + "= Third section =\nSection 3 content",
+            "xwiki/2.0");        
+    }
+
+    /**
+     * Verify edit section is working in wiki editor (xwiki/1.0). XWIKI-174 : Sectional editing.
+     */
+    public void testSectionEditInWikiEditor()
+    {
+        initXWiki10Env();
         clickLinkWithLocator("//div[@id='xwikicontent']/span[2]/a"); // Edit the second section
         clickLinkWithText("Wiki");
         assertTextNotPresent("First section");
@@ -46,26 +61,25 @@ public class SectionTest extends AbstractXWikiTestCase
     }
 
     /**
-     * Verify edit section is working in wysiwyg editor (xwiki/1.0).
-     * XWIKI-174 : Sectional editing.
+     * Verify edit section is working in wysiwyg editor (xwiki/1.0). XWIKI-174 : Sectional editing.
      */
     public void testSectionEditInWysiwygEditor()
     {
-        open("Test", "SectionEditing");
+        initXWiki10Env();
         clickLinkWithLocator("//div[@id='xwikicontent']/span[3]/a"); // Edit the subsection
         assertTextNotPresent("First section");
         assertTextNotPresent("Second section");
         assertTextPresent("Subsection");
-        assertTextNotPresent("Third section");        
+        assertTextNotPresent("Third section");
     }
-    
+
     /**
-     * Verify section save does not override the whole document content (xwiki/1.0).
-     * XWIKI-4033: When saving after section edit entire page is overwritten.
+     * Verify section save does not override the whole document content (xwiki/1.0). XWIKI-4033: When saving after
+     * section edit entire page is overwritten.
      */
     public void testSectionSaveDoesNotOverrideTheWholeContent()
     {
-        open("Test", "SectionEditing");
+        initXWiki10Env();
         clickLinkWithLocator("//div[@id='xwikicontent']/span[3]/a"); // Edit the subsection
         clickLinkWithText("Wiki");
         clickEditSaveAndView();
@@ -77,19 +91,11 @@ public class SectionTest extends AbstractXWikiTestCase
     }
 
     /**
-     * Verify edit section is working in wiki editor (xwiki/2.0).
-     * XWIKI-2881 : Implement Section editing.
+     * Verify edit section is working in wiki editor (xwiki/2.0). XWIKI-2881 : Implement Section editing.
      */
     public void testSectionEditInWikiEditor_syntax20()
     {
-        createPage("Test", "SectionEditingIncluded", 
-            "== Included section ==\nFirst Included section content\n{{velocity wiki=true}}\n"
-                + "#foreach($h in ['First', 'Second'])\n== $h generated section ==\n\n$h generated paragraph\n"
-                + "#end\n{{velocity}}\n");
-        createPage("Test", "SectionEditing20", "= First section =\nSection 1 content\n\n"
-            + "= Second section =\nSection 2 content\n\n== Subsection ==\nSubsection content\n\n"
-            + "{{include document='Test.SectionEditingIncluded'/}}\n\n" + "= Third section =\nSection 3 content",
-            "xwiki/2.0");    
+        initXWiki20Env();
         clickLinkWithLocator("//div[@id='xwikicontent']/span[2]/a"); // Edit the second section
         clickLinkWithText("Wiki");
         assertTextNotPresent("First section");
@@ -99,26 +105,25 @@ public class SectionTest extends AbstractXWikiTestCase
     }
 
     /**
-     * Verify edit section is working in wysiwyg editor (xwiki/2.0).
-     * XWIKI-2881 : Implement Section editing.
+     * Verify edit section is working in wysiwyg editor (xwiki/2.0). XWIKI-2881 : Implement Section editing.
      */
     public void testSectionEditInWysiwygEditor_syntax20()
     {
-        open("Test", "SectionEditing20");        
+        initXWiki20Env();
         clickLinkWithLocator("//div[@id='xwikicontent']/span[4]/a"); // Edit the last section
         assertTextNotPresent("First section");
         assertTextNotPresent("Second section");
         assertTextNotPresent("Subsection");
-        assertTextPresent("Third section");        
+        assertTextPresent("Third section");
     }
-    
+
     /**
-     * Verify section save does not override the whole document content (xwiki/2.0).
-     * XWIKI-4033: When saving after section edit entire page is overwritten.
+     * Verify section save does not override the whole document content (xwiki/2.0). XWIKI-4033: When saving after
+     * section edit entire page is overwritten.
      */
     public void testSectionSaveDoesNotOverrideTheWholeContent_syntax20()
-    {        
-        open("Test", "SectionEditing20");
+    {
+        initXWiki20Env();
         clickLinkWithLocator("//div[@id='xwikicontent']/span[4]/a"); // Edit the last section
         clickLinkWithText("Wiki");
         clickEditSaveAndView();

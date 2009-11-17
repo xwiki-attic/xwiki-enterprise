b177d1df4f659f07e04266b2fd20ea9ca3d17f02
mflorea <mariusdumitru.florea@xwiki.com>
2009-11-17 23:46:17 +0000

    XWIKI-4581: Change the name for "Background Color" to "Highlight Color"
    Merged from trunk, revision 25112.
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/branches/xwiki-enterprise-2.0@25113 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/wysiwyg-tests/src/test/it/com/xpn/xwiki/it/selenium/ColorTest.java
+++ distribution-test/wysiwyg-tests/src/test/it/com/xpn/xwiki/it/selenium/ColorTest.java
@@ -173,7 +173,7 @@ public class ColorTest extends AbstractWysiwygTestCase
      */
     protected void clickBackgroundColorButton()
     {
-        pushToolBarButton("Background Color");
+        pushToolBarButton("Highlight Color");
     }
 
     /**

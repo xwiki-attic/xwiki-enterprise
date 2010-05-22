70c942eefc6e0da93e3456eb28803a40b0ba7c8b
jvelo <jerome.velociter@gmail.com>
2010-05-22 14:26:07 +0000

    XASCH-54 Default edit mode for jobs is "edit" when it should be inline
    UI test
    
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/trunk@28965 f329d543-caf0-0310-9063-dda96c69346f

---
+++ distribution-test/ui-tests/src/test/it/org/xwiki/it/ui/elements/SchedulerJobInlinePage.java
@@ -0,0 +1,24 @@
+package org.xwiki.it.ui.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Represents a scheduler job edited in inline mode.
+ * 
+ * @since 2.3.1
+ * @since 2.4M1
+ * @version $Id$
+ */
+public class SchedulerJobInlinePage extends InlinePage
+{
+    public SchedulerJobInlinePage(WebDriver driver)
+    {
+        super(driver);
+    }
+
+    public boolean isQuartzDocumentationReferenced()
+    {
+        return getDriver().findElement(By.partialLinkText("official quartz scheduler documentation")) != null;
+    }
+}

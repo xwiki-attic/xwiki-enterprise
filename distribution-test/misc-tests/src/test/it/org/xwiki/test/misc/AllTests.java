c854f9e41df4af2d2efd9919932dc03da39f8b6d
vmassol <vincent@massol.net>
2010-10-18 12:54:07 +0000

    XE-725: Rename Maven Group Ids and packages from com.xpn.* to org.xwiki.enterprise
    * Step 2: Migrate package names. Done for misc-tests
    
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/trunk@31931 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/misc-tests/src/test/it/org/xwiki/test/misc/AllTests.java
+++ distribution-test/misc-tests/src/test/it/org/xwiki/test/misc/AllTests.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package com.xpn.xwiki.it;
+package org.xwiki.test.misc;
 
 import junit.framework.Test;
 import junit.framework.TestCase;

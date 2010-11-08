e8de2a8afb589748ea7179e67176e027d962f265
vmassol <vincent@massol.net>
2010-11-08 09:35:06 +0000

    Fixed another error I introduced in previous commit
    git-svn-id: https://svn.xwiki.org/svnroot/xwiki/enterprise/trunk@32605 f329d543-caf0-0310-9063-dda96c69346f

--- distribution-test/ui-tests/src/test/it/org/xwiki/test/ui/BreadcrumbsTest.java
+++ distribution-test/ui-tests/src/test/it/org/xwiki/test/ui/BreadcrumbsTest.java
@@ -64,7 +64,7 @@ public class BreadcrumbsTest extends AbstractAdminAuthenticatedTest
         // Remove view rights on the Test.ParentPage page to everyone except Admin user so that we can verify that the
         // breadcrumb of the child page doesn't display pages for which you don't have view rights to.
         rep.switchToUsers();
-        rep.setRight("XWikiAdminGroup", Right.VIEW, State.ALLOW);
+        rep.setRight("Admin", Right.VIEW, State.ALLOW);
         // Log out...
         getUtil().setSession(null);
 

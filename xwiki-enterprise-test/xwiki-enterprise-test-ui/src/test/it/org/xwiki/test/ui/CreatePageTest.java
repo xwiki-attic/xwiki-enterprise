661376e07504d69abba9fe878b2a973bd1d5fa92
cjdelisle <calebdelisle@lavabit.com>
2011-05-26 10:42:22 -0400

    Revert "[misc] Ignored a test in an attempt to find what is breaking the test run on jenkins." That wasn't it. This reverts commit 2c982c5884dabbf4e5cce84ea37ff0556a3e9d2c.

--- xwiki-enterprise-test/xwiki-enterprise-test-ui/src/test/it/org/xwiki/test/ui/CreatePageTest.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-ui/src/test/it/org/xwiki/test/ui/CreatePageTest.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -94,8 +93,6 @@ public class CreatePageTest extends AbstractAdminAuthenticatedTest
     /**
      * Tests if a new page can be created from a template.
      */
-    // Ignored because this test seems to be freezing up on the ci server and breaking all tests.
-    @Ignore
     @Test
     public void testCreatePageFromTemplate()
     {

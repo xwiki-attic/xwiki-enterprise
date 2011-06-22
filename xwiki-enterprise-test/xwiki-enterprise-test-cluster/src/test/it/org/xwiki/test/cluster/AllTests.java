ef663b80921f63be110b1a24164e7171ca40abc7
tmortagne <thomas.mortagne@gmail.com>
2011-06-22 16:35:26 +0200

    XWIKI-6718: Remote observation should fallback on JGroups embedded configurations Fix tests

--- xwiki-enterprise-test/xwiki-enterprise-test-cluster/src/test/it/org/xwiki/test/cluster/AllTests.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-cluster/src/test/it/org/xwiki/test/cluster/AllTests.java
@@ -19,8 +19,6 @@
  */
 package org.xwiki.test.cluster;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
 
@@ -30,20 +28,18 @@ import org.xwiki.test.integration.XWikiExecutorSuite;
 
 /**
  * Runs all functional tests found in the classpath and start/stop XWiki before/after the tests (only once).
- *
+ * 
  * @version $Id$
  */
 @RunWith(XWikiExecutorSuite.class)
 @XWikiExecutorSuite.Executors(2)
 public class AllTests
 {
-    private static final String WEBINF_PATH = "/observation/remote/jgroups";
-
     @XWikiExecutorSuite.PreStart
     public void preInitialize(List<XWikiExecutor> executors) throws Exception
     {
-        initChannel(executors.get(0), "tcp1");
-        initChannel(executors.get(1), "tcp2");
+        initChannel(executors.get(0), "tcp");
+        initChannel(executors.get(1), "tcp");
     }
 
     private void initChannel(XWikiExecutor executor, String channelName) throws Exception
@@ -53,19 +49,7 @@ public class AllTests
         properties.setProperty("observation.remote.channels", channelName);
         executor.saveXWikiProperties(properties);
 
-        String filename = channelName + ".xml";
-
-        InputStream is = getClass().getResourceAsStream("/" + filename);
-        try {
-            FileOutputStream fos = new FileOutputStream(executor.getWebInfDirectory() + WEBINF_PATH + "/" + filename);
-
-            byte[] buffer = new byte[1024];
-
-            for (int nb; (nb = is.read(buffer)) > 0;) {
-                fos.write(buffer, 0, nb);
-            }
-        } finally {
-            is.close();
-        }
+        // This is sometimes required sometime not depending on the system environment
+        executor.addEnv("jgroups.bind_addr", "localhost");
     }
 }

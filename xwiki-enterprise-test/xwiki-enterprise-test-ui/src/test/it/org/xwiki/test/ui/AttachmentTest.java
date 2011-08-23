975616afab8bf96610aae777f044cb4de877db19
Vincent Massol <vincent@massol.net>
2011-08-23 15:27:19 +0200

    typo

--- xwiki-enterprise-test/xwiki-enterprise-test-ui/src/test/it/org/xwiki/test/ui/AttachmentTest.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-ui/src/test/it/org/xwiki/test/ui/AttachmentTest.java
@@ -74,7 +74,7 @@ public class AttachmentTest extends AbstractAdminAuthenticatedTest
 
         // Note: With Firefox 3.x the following click() generates 2 calls to the server side at once which can cause
         // concurrency issues. Using Firefox 6.0 (and possibly other versions) generates only a single call, as it
-        // shoud be. I've opened http://code.google.com/p/selenium/issues/detail?id=2320
+        // should be. I've opened http://code.google.com/p/selenium/issues/detail?id=2320
         ap.getAttachmentLink(this.testAttachment).click();
 
         Assert.assertEquals("This is a small attachment.", getDriver().findElement(By.tagName("html")).getText());

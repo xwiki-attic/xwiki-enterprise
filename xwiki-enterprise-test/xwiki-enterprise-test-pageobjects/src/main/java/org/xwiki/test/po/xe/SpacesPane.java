56a896e0b0630ad9d8d7348320ae2f31a71929e5
Marius Dumitru Florea <marius@xwiki.com>
2012-06-07 12:41:59 +0300

    XWIKI-7855: Automatically hide technical pages created by AppWithinMinutes * Add functional test. * Move SpaceIndexPage page object to platform-index-test-pageobjects. * Add dependency to platform-index-test-pageobjects.

--- xwiki-enterprise-test/xwiki-enterprise-test-pageobjects/src/main/java/org/xwiki/test/po/xe/SpacesPane.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-pageobjects/src/main/java/org/xwiki/test/po/xe/SpacesPane.java
@@ -22,6 +22,7 @@ package org.xwiki.test.po.xe;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.xwiki.index.test.po.SpaceIndexPage;
 import org.xwiki.test.ui.po.BaseElement;
 import org.xwiki.test.ui.po.editor.WYSIWYGEditPage;
 

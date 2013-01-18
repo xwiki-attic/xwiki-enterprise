ec4a72d9f9d1a25da874310ec206818cd3902b53
tmortagne <thomas.mortagne@gmail.com>
2013-01-18 15:17:26 +0100

    Add test for XWIKI-8710

--- xwiki-enterprise-test/xwiki-enterprise-test-misc/src/test/it/org/xwiki/test/misc/HTMLExportTest.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-misc/src/test/it/org/xwiki/test/misc/HTMLExportTest.java
@@ -34,9 +34,50 @@ public class HTMLExportTest extends TestCase
     /**
      * Verify that the HTML export feature works on a single simple page by downloading the generated Zip.
      */
-    public void testHTMLExport() throws Exception
+    public void testHTMLExportCurrentPage() throws Exception
     {
         URL url = new URL("http://localhost:8080/xwiki/bin/export/Main/WebHome?format=html");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        InputStream is = connection.getInputStream();
+        ZipInputStream zis = new ZipInputStream(is);
+
+        boolean foundWebHome = false;
+        boolean foundResources = false;
+
+        // We must read the full stream as otherwise if we close it before we've fully read it
+        // then the server side will get a broken pipe since it's still trying to send data on it.
+        for (ZipEntry entry; (entry = zis.getNextEntry()) != null; zis.closeEntry()) {
+            if (entry.getName().equals("xwiki.Main.WebHome.html")) {
+                String content = IOUtils.toString(zis);
+
+                // Verify that the content was rendered properly
+                assertTrue("Should have contained 'Welcome to your wiki'", content.contains("Welcome to your wiki"));
+
+                // Ensure that the translations have been rendered properly
+                assertFalse("$msg should have been expanded", content.contains("$msg"));
+
+                foundWebHome = true;
+            } else if (entry.getName().startsWith("resources/")) {
+                foundResources = true;
+            } else {
+                IOUtils.readLines(zis);
+            }
+        }
+
+        assertTrue("Failed to find xwiki.Main.WebHome.html entry", foundWebHome);
+        assertTrue("Failed to find resource folder entry", foundResources);
+
+        zis.close();
+    }
+
+    /**
+     * Verify that the HTML export feature works on a passed patern instead of an explicit page name or the current page.
+     */
+    public void testHTMLExportPattern() throws Exception
+    {
+        URL url = new URL("http://localhost:8080/xwiki/bin/export/UnexistingSpace/UnexistingPage?format=html&pages=Main.WebH%25");
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 

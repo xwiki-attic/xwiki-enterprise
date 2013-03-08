88e276a413f25664be4da7293611818f435b80c3
Vincent Massol <vincent@massol.net>
2013-03-08 16:39:57 +0100

    [Misc] Started moving Administration App functional tests from enterprise to platform. As a consequence I also had to: * Move a panel test that was wrongly located in Administration functional tests to the platform panel app * Create a new AbstractStandardUserAuthenticatedTest class to help write functional tests that need a standard user (non admin) * Redispatch tests that were in Admin Test but should not have been there...

--- xwiki-enterprise-test/xwiki-enterprise-test-ui/src/test/it/org/xwiki/test/ui/administration/AdministrationTest.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-ui/src/test/it/org/xwiki/test/ui/administration/AdministrationTest.java
@@ -19,16 +19,11 @@
  */
 package org.xwiki.test.ui.administration;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
-import org.xwiki.administration.test.po.AdministrablePage;
 import org.xwiki.administration.test.po.AdministrationPage;
-import org.xwiki.administration.test.po.PageElementsAdministrationSectionPage;
-import org.xwiki.panels.test.po.PageWithPanels;
-import org.xwiki.panels.test.po.PanelEditPage;
-import org.xwiki.panels.test.po.PanelsHomePage;
 import org.xwiki.test.ui.AbstractAdminAuthenticatedTest;
+
+import junit.framework.Assert;
 
 /**
  * Verify the overall Administration application features.
@@ -39,94 +34,24 @@ import org.xwiki.test.ui.AbstractAdminAuthenticatedTest;
 public class AdministrationTest extends AbstractAdminAuthenticatedTest
 {
     /**
-     * This method makes the following tests :
-     *
-     * <ul>
-     * <li>Login as global admin.</li>
-     * <li>Validate presence of default sections for global and space sections.</li>
-     * <li>Validate presence of application administration sections at global level only.</li>
-     * </ul>
+     * TODO: Remove this test when the MessageStream Admin UI has been moved from Enterprise to Platform.
      */
     @Test
     public void globalAndSpaceSections()
     {
-        // Go to any page. Note that we go to a not existing page for 2 reasons:
-        // - verify that it has a menu action to administer the wiki
-        // - (more importantly) it's faster than going to the wiki's home page which will take longer to display... ;)
-        getUtil().gotoPage(getTestClassName(), getTestMethodName());
-        AdministrablePage page = new AdministrablePage();
-        AdministrationPage administrationPage = page.clickAdministerWiki();
+        AdministrationPage administrationPage = AdministrationPage.gotoPage();
 
-        // TODO: Move these tests in their own modules, i.e. the modules that brought the Admin UI extension
-        administrationPage.hasSection("Editing");
-        administrationPage.hasSection("Localization");
-        administrationPage.hasSection("Email");
-        administrationPage.hasSection("Presentation");
-        administrationPage.hasSection("Elements");
-        administrationPage.hasSection("Registration");
-        administrationPage.hasSection("Users");
-        administrationPage.hasSection("Groups");
-        administrationPage.hasSection("Rights");
-        administrationPage.hasSection("Registration");
-        administrationPage.hasSection("Import");
-        administrationPage.hasSection("Export");
-        administrationPage.hasSection("Templates");
-        administrationPage.hasSection("MessageStream");
-        administrationPage.hasSection("Panels.PanelWizard");
+        Assert.assertTrue(administrationPage.hasSection("MessageStream"));
 
-        // Select space administration (Main space)
-        AdministrationPage spaceAdministrationPage = administrationPage.selectSpaceToAdminister("Main");
+        // Select space administration (XWiki space, since that space exists)
+        AdministrationPage spaceAdministrationPage = administrationPage.selectSpaceToAdminister("XWiki");
 
         // Note: I'm not sure this is good enough since waitUntilPageIsLoaded() tests for the existence of the footer
         // but if the page hasn't started reloading then the footer will be present... However I ran this test 300
         // times in a row without any failure...
         spaceAdministrationPage.waitUntilPageIsLoaded();
 
-        spaceAdministrationPage.hasSection("Presentation");
-        spaceAdministrationPage.hasSection("Elements");
-        spaceAdministrationPage.hasSection("Rights");
-        spaceAdministrationPage.hasSection("Panels.PanelWizard");
-
         // All those sections should not be present
-        spaceAdministrationPage.hasNotSection("Editing");
-        spaceAdministrationPage.hasNotSection("Localization");
-        spaceAdministrationPage.hasNotSection("Email");
-        spaceAdministrationPage.hasNotSection("Registration");
-        spaceAdministrationPage.hasNotSection("Users");
-        spaceAdministrationPage.hasNotSection("Groups");
-        spaceAdministrationPage.hasNotSection("Registration");
-        spaceAdministrationPage.hasNotSection("Import");
-        spaceAdministrationPage.hasNotSection("Export");
-        spaceAdministrationPage.hasNotSection("Templates");
-        spaceAdministrationPage.hasNotSection("MessageStream");
-    }
-
-    /**
-     * @see "XWIKI-8591: Cannot use a panel with a name containing spaces"
-     */
-    @Test
-    public void addPanelWithSpacesInName()
-    {
-        // Create a panel whose name contain spaces.
-        String panelName = "My First Panel";
-        getUtil().deletePage("Panels", panelName);
-        PanelEditPage panelEditPage = PanelsHomePage.gotoPage().createPanel(panelName);
-        panelEditPage.setContent(String.format(PanelEditPage.DEFAULT_CONTENT_FORMAT, panelName, getTestMethodName()));
-        panelEditPage.clickSaveAndContinue();
-
-        // Add the panel to the right column from the administration.
-        PageElementsAdministrationSectionPage pageElements =
-            new AdministrablePage().clickAdministerWiki().clickPageElementsSection();
-        String rightPanels = pageElements.getRightPanels();
-        pageElements.setRightPanels(rightPanels + ",Panels." + panelName);
-        try {
-            pageElements.clickSave();
-            Assert.assertTrue(new PageWithPanels().hasPanel(panelName));
-        } finally {
-            // Restore the right panels.
-            pageElements = PageElementsAdministrationSectionPage.gotoPage();
-            pageElements.setRightPanels(rightPanels);
-            pageElements.clickSave();
-        }
+        Assert.assertTrue(spaceAdministrationPage.hasNotSection("MessageStream"));
     }
 }

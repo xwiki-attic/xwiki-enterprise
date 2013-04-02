ba03e183f35343507d5de3344f7706a499477de8
tmortagne <thomas.mortagne@gmail.com>
2013-04-02 12:05:19 +0200

    Fix comment

--- xwiki-enterprise-test/xwiki-enterprise-test-rest/src/test/it/org/xwiki/test/rest/WikisResourceTest.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-rest/src/test/it/org/xwiki/test/rest/WikisResourceTest.java
@@ -113,8 +113,8 @@ public class WikisResourceTest extends AbstractHttpTest
             checkLinks(searchResult);
         }
 
-        // Note: we use $msg.get(...) a bit everywhere in our title for the moment... The search is a search in the DB
-        // and not on the rendered content. Thus for our tests we search on msg...
+        // Note: we use $services.<service> a bit everywhere in our title for the moment... The search is a search in the DB
+        // and not on the rendered content. Thus for our tests we search on services...
         getMethod =
             executeGet(String.format("%s?q=services&scope=title", getUriBuilder(WikiSearchResource.class).build(getWiki())));
         Assert.assertEquals(getHttpMethodInfo(getMethod), HttpStatus.SC_OK, getMethod.getStatusCode());

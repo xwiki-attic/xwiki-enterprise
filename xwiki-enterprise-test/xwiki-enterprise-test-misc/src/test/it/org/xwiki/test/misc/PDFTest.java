b5a37f278c32cbb0c8653f127363b2ce00de2f99
Marius Dumitru Florea <marius@xwiki.com>
2013-08-02 14:38:18 +0300

    XWIKI-9370: PDF Export doesn't list the Table of Contents under certain circumstances * Add functional test.

--- xwiki-enterprise-test/xwiki-enterprise-test-misc/src/test/it/org/xwiki/test/misc/PDFTest.java
+++ xwiki-enterprise-test/xwiki-enterprise-test-misc/src/test/it/org/xwiki/test/misc/PDFTest.java
@@ -27,24 +27,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import junit.framework.TestCase;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.interactive.action.type.PDAction;
+import org.apache.pdfbox.pdmodel.interactive.action.type.PDActionGoTo;
 import org.apache.pdfbox.pdmodel.interactive.action.type.PDActionURI;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDDestination;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageDestination;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageXYZDestination;
 import org.apache.pdfbox.util.PDFText2HTML;
 import org.apache.pdfbox.util.PDFTextStripperByArea;
-
-import junit.framework.TestCase;
 
 public class PDFTest extends TestCase
 {
     /**
      * Verify that the PDF export feature works on a single simple page by downloading the PDF and parsing it using
      * PDFBox.
-     *
+     * 
      * @see "XWIKI-7048: PDF export templates can display properties of other objects if the XWiki.PDFClass object is
      *      missing"
      */
@@ -61,7 +65,7 @@ public class PDFTest extends TestCase
 
     /**
      * Verify that we can export content having links to attachments.
-     *
+     * 
      * @see "XWIKI-8978: PDF Export does not handle XWiki links to attached files properly"
      */
     public void testExportContentWithAttachmentLink() throws Exception
@@ -71,6 +75,24 @@ public class PDFTest extends TestCase
         assertTrue(urls.containsKey("XWikiLogo.png"));
         assertEquals("http://localhost:8080/xwiki/bin/download/Sandbox/WebHome/XWikiLogo.png",
             urls.get("XWikiLogo.png"));
+    }
+
+    /**
+     * Verify the PDF export with table of contents.
+     * 
+     * @see "XWIKI-9370: PDF Export doesn't list the Table of Contents under certain circumstances"
+     */
+    public void testTableOfContents() throws Exception
+    {
+        Map<String, String> internalLinks =
+            extractToCLinks(new URL("http://localhost:8080/xwiki/bin/export/Sandbox/WebHome"
+                + "?format=pdf&pdftoc=1&attachments=1&pdfcover=0"), 0);
+        // Make sure we have a Table of Contents.
+        assertTrue(internalLinks.containsKey("Mixed list"));
+        // Make sure the Table of Contents links point to their corresponding heading.
+        for (Map.Entry<String, String> entry : internalLinks.entrySet()) {
+            assertTrue(entry.getValue().contains(entry.getKey()));
+        }
     }
 
     private String getPDFContent(URL url) throws Exception
@@ -85,65 +107,129 @@ public class PDFTest extends TestCase
         return text;
     }
 
-    /**
-     * Code copied from http://www.docjar.com/html/api/org/apache/pdfbox/examples/pdmodel/PrintURLs.java.html
-     */
     private Map<String, String> extractURLs(URL url) throws Exception
     {
         Map<String, String> urls = new HashMap<String, String>();
-        PDDocument doc = null;
+        PDDocument document = null;
         try {
-
-            doc = PDDocument.load(url);
-            List allPages = doc.getDocumentCatalog().getAllPages();
-            for (int i = 0; i < allPages.size(); i++) {
-                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
-                PDPage page = (PDPage) allPages.get(i);
-                List annotations = page.getAnnotations();
-                //first setup text extraction regions
-                for (int j = 0; j < annotations.size(); j++) {
-                    PDAnnotation annot = (PDAnnotation) annotations.get(j);
-                    if (annot instanceof PDAnnotationLink) {
-                        PDAnnotationLink link = (PDAnnotationLink) annot;
-                        PDRectangle rect = link.getRectangle();
-                        //need to reposition link rectangle to match text space
-                        float x = rect.getLowerLeftX();
-                        float y = rect.getUpperRightY();
-                        float width = rect.getWidth();
-                        float height = rect.getHeight();
-                        int rotation = page.findRotation();
-                        if (rotation == 0) {
-                            PDRectangle pageSize = page.findMediaBox();
-                            y = pageSize.getHeight() - y;
-                        } else if (rotation == 90) {
-                            //do nothing
-                        }
-
-                        Rectangle2D.Float awtRect = new Rectangle2D.Float(x, y, width, height);
-                        stripper.addRegion("" + j, awtRect);
-                    }
-                }
-
-                stripper.extractRegions(page);
-
-                for (int j = 0; j < annotations.size(); j++) {
-                    PDAnnotation annot = (PDAnnotation) annotations.get(j);
-                    if (annot instanceof PDAnnotationLink) {
-                        PDAnnotationLink link = (PDAnnotationLink) annot;
-                        PDAction action = link.getAction();
-                        String urlText = stripper.getTextForRegion("" + j);
-                        if (action instanceof PDActionURI) {
-                            PDActionURI uri = (PDActionURI) action;
-                            urls.put(urlText.trim(), uri.getURI());
-                        }
-                    }
+            document = PDDocument.load(url);
+            for (Map.Entry<String, PDAction> entry : extractLinks(document).entrySet()) {
+                if (entry.getValue() instanceof PDActionURI) {
+                    PDActionURI uri = (PDActionURI) entry.getValue();
+                    urls.put(entry.getKey(), uri.getURI());
                 }
             }
         } finally {
-            if (doc != null) {
-                doc.close();
+            if (document != null) {
+                document.close();
             }
         }
         return urls;
+    }
+
+    private Map<String, String> extractToCLinks(URL url, int tocPageIndex) throws Exception
+    {
+        Map<String, String> internalLinks = new HashMap<String, String>();
+        PDDocument document = null;
+        try {
+            document = PDDocument.load(url);
+            PDPage tocPage = (PDPage) document.getDocumentCatalog().getAllPages().get(tocPageIndex);
+            for (Map.Entry<String, PDAction> entry : extractLinks(tocPage).entrySet()) {
+                if (entry.getValue() instanceof PDActionGoTo) {
+                    PDActionGoTo anchor = (PDActionGoTo) entry.getValue();
+                    internalLinks.put(entry.getKey(), getDestinationText(anchor.getDestination()));
+                }
+            }
+        } finally {
+            if (document != null) {
+                document.close();
+            }
+        }
+        return internalLinks;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, PDAction> extractLinks(PDDocument document) throws Exception
+    {
+        Map<String, PDAction> links = new HashMap<String, PDAction>();
+        for (PDPage page : (List<PDPage>) document.getDocumentCatalog().getAllPages()) {
+            links.putAll(extractLinks(page));
+        }
+        return links;
+    }
+
+    /**
+     * Code adapted from http://www.docjar.com/html/api/org/apache/pdfbox/examples/pdmodel/PrintURLs.java.html
+     */
+    private Map<String, PDAction> extractLinks(PDPage page) throws Exception
+    {
+        Map<String, PDAction> links = new HashMap<String, PDAction>();
+        PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+        List<PDAnnotation> annotations = page.getAnnotations();
+        // First setup the text extraction regions.
+        for (int j = 0; j < annotations.size(); j++) {
+            PDAnnotation annotation = annotations.get(j);
+            if (annotation instanceof PDAnnotationLink) {
+                PDAnnotationLink link = (PDAnnotationLink) annotation;
+                PDRectangle rect = link.getRectangle();
+                // Need to reposition link rectangle to match text space.
+                float x = rect.getLowerLeftX();
+                float y = rect.getUpperRightY();
+                float width = rect.getWidth();
+                float height = rect.getHeight();
+                int rotation = page.findRotation();
+                if (rotation == 0) {
+                    PDRectangle pageSize = page.findMediaBox();
+                    y = pageSize.getHeight() - y;
+                } else if (rotation == 90) {
+                    // Do nothing.
+                }
+
+                Rectangle2D.Float awtRect = new Rectangle2D.Float(x, y, width, height);
+                stripper.addRegion(String.valueOf(j), awtRect);
+            }
+        }
+
+        stripper.extractRegions(page);
+
+        for (int j = 0; j < annotations.size(); j++) {
+            PDAnnotation annotation = annotations.get(j);
+            if (annotation instanceof PDAnnotationLink) {
+                PDAnnotationLink link = (PDAnnotationLink) annotation;
+                String label = stripper.getTextForRegion(String.valueOf(j)).trim();
+                links.put(label, link.getAction());
+            }
+        }
+
+        return links;
+    }
+
+    private String getDestinationText(PDDestination destination) throws Exception
+    {
+        if (destination instanceof PDPageXYZDestination) {
+            return getDestinationText((PDPageXYZDestination) destination);
+        } else if (destination instanceof PDPageDestination) {
+            return "Page " + ((PDPageDestination) destination).getPageNumber();
+        }
+        return destination.toString();
+    }
+
+    private String getDestinationText(PDPageXYZDestination destination) throws Exception
+    {
+        PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+        stripper.addRegion("destination", getRectangleBelowDestination(destination));
+        stripper.extractRegions(destination.getPage());
+        return stripper.getTextForRegion("destination").trim();
+    }
+
+    private Rectangle2D getRectangleBelowDestination(PDPageXYZDestination destination)
+    {
+        PDPage page = destination.getPage();
+        PDRectangle pageSize = page.findMediaBox();
+        float x = destination.getLeft();
+        float y = pageSize.getHeight() - destination.getTop();
+        float width = pageSize.getWidth();
+        float height = destination.getTop();
+        return new Rectangle2D.Float(x, y, width, height);
     }
 }
